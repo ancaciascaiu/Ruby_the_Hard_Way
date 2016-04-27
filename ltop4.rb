095a02966752d0493ca662ea6261a30c004d56c1
Anca Ciascaiu <anca_ciascaiu@yahoo.com>
2016-04-26 19:49:48 -0500

    practice

---
+++ ltop4.rb
@@ -0,0 +1,5 @@
+puts "Table of contents".center(40)
+puts ""
+puts "Chapter 1: Getting Started".ljust(20) + "page  1".rjust(14)
+puts "Chapter 2: Numbers".ljust(20) + "page  9".rjust(20)
+puts "Chapter 3: Letters".ljust(20) + "page 13".rjust(20)
