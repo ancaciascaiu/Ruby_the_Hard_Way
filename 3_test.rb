5e6b6cca11421c17ebad9a04e8cd8fb164812e4a
Anca Fodor Ciascaiu <anca_ciascaiu@yahoo.com>
2016-06-03 21:09:51 -0500

    define and call method

---
+++ 3_test.rb
@@ -0,0 +1,20 @@
+# 1. Write a method that adds
+#    two strings together.
+
+# 2. Count up to 20, printing each
+#    number and whether it is even
+#    or odd.
+
+# 3. Write a method that asks the user
+#    for their age, then says their age
+#    ain't nothing but a number.
+#    example: User enters "46"
+#    output: "46 ain't nothing but a number!"
+
+
+#1
+def adstr(a, b)
+    puts a + b
+end
+adstr("Anca", "Silviu")
+
