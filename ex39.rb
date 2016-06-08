dfdea6f2433d2abc9066ec429b7f3c5ee9b85386
Anca Fodor Ciascaiu <anca_ciascaiu@yahoo.com>
2016-06-07 19:44:20 -0500

    set states

---
+++ ex39.rb
@@ -0,0 +1,10 @@
+require './ex39_module.rb'
+
+# create a mapping of state to abbreviation
+states = Dict.new()
+Dict.set(states, 'Oregon', 'OR')
+Dict.set(states, 'Florida', 'FL')
+Dict.set(states, 'California', 'CA')
+Dict.set(states, 'New York', 'NY')
+Dict.set(states, 'Michigan', 'MI')
+
