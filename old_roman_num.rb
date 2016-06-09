3fea20e8f5ee8c2a26c498395ecec29dbd7056f3
Anca Fodor Ciascaiu <anca_ciascaiu@yahoo.com>
2016-06-09 16:13:22 -0500

    it works for thous, hundr, tens and ones

--- old_roman_num.rb
+++ old_roman_num.rb
@@ -1,3 +1,4 @@
+
 #Write a method that when passed an integer between 1 and 3000 returns a string containing the old roman numeral.
 
 #puts "Please eneter a number that you want to convert to roman numeral:"
@@ -7,14 +8,15 @@ def old_roman number
 	
 	ro_nr = ""
 	
-	if number/1000
+	if number/1000 == true
 		thous = "M" * (number/1000)
 		ro_nr += thous
+		number = number - (number.to_s[0].to_i*1000)
 	end
 	
-	number = number - (number.to_s[0].to_i*1000)
 	
-	if number /100
+	
+	if number /100 > 0
 		if number/100 > 5
 			hun = "D" + ("C" * (number/100-5))
 		elsif number/100 == 5
@@ -23,11 +25,10 @@ def old_roman number
 			hun = "C" * (number/100)
 		end
 		ro_nr += hun
+		number = number - (number.to_s[0].to_i*100)
 	end
 	
-	number = number - (number.to_s[0].to_i*100)
-	
-	if number/10
+	if number/10 > 0
 		if number/10 > 5
 			tens = "L" + ("X" * (number/10-5))
 		elsif number/10 == 5
@@ -36,11 +37,12 @@ def old_roman number
 			tens = "X" * (number/10)
 		end
 		ro_nr += tens
+		number = number - (number.to_s[0].to_i*10)
 	end
 	
-	number = number - (number.to_s[0].to_i*10)
 	
-	if number/1
+	
+	if number/1 > 0
 		if number > 5
 			ones = "V" + ("I" * (number-5))
 		elsif number == 5
@@ -55,11 +57,19 @@ def old_roman number
 
 end
 
-old_roman 2864
+
 old_roman 9978
 old_roman 5555
 old_roman 3333
 old_roman 234
+old_roman 555
+old_roman 789
+old_roman 31
+old_roman 55
+old_roman 67
+old_roman 3
+old_roman 5
+old_roman 7
 	
 
 
